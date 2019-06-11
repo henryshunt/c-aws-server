a52d2b4da1795fd6580883aba7d7b5e7a4c2acec
Henry Hunt <henryhunt.dev@gmail.com>
2019-06-11 18:20:20 +0100

    Reimplemented station data file

--- data/station.php
+++ data/station.php
@@ -1,49 +1,62 @@
 <?php
 date_default_timezone_set("UTC");
-include_once("config.php");
-include_once("database.php");
-include_once("queries.php");
+include_once("../routines/config.php");
+include_once("../routines/database.php");
+include_once("../routines/analysis.php");
 
-$data = array_fill_keys(array("Time", "EncT", "CPUT"), null);
+$data = array_fill_keys(["Time", "EncT", "CPUT"], null);
 
-// Try parsing time specified in URL
-if (isset($_GET["time"]) == false) { echo json_encode($data); exit(); }
-try {
-    $url_time = date_create_from_format("Y-m-d\TH-i-00", $_GET["time"]);
+$config = new Config("../config.ini");
+if (!$config) { echo json_encode($data); exit(); }
+$pdo = new_db_conn($config);
+if (!$pdo) { echo json_encode($data); exit(); }
+
+// Parse time specified in URL
+if (isset($_GET["time"]))
+{
+    try
+    {
+        $url_time = date_create_from_format(
+            "Y-m-d\TH-i-s", $_GET["time"]);
+    }
+    catch (Exception $e) { echo json_encode($data); exit(); }
 }
-catch(Exception $e) { echo json_encode($data); exit(); }
+else { echo json_encode($data); exit(); }
 
-if ($db_conn) {
-    // Get record for that time
-    $record = $db_conn -> query(sprintf(
-        $SELECT_SINGLE_ENVREPORT, $url_time -> format("Y-m-d H:i:s")));
-    
-    if ($record) {
-        if ($record -> num_rows == 0) {
-            // Go back a minute if no record and not in absolute mode
-            if (!isset($_GET["abs"])) {
-                $url_time -> sub(new DateInterval("PT1M"));
-                $record = $db_conn -> query(sprintf(
-                    $SELECT_SINGLE_ENVREPORT, $url_time -> format("Y-m-d H:i:s")));
+// Get record for specified time
+$result = record_for_time($pdo, $url_time, DbTable::ENVREPORTS);
 
-                if ($record) {
-                    if ($record -> num_rows == 1) {
-                        // Add record data to final data
-                        foreach ($row = $record -> fetch_assoc() as $key => $value) {
-                            if (array_key_exists($key, $data)) { $data[$key] = $value; }
-                        }
-                    } else { $url_time -> add(new DateInterval("PT1M")); }
-                } else { $url_time -> add(new DateInterval("PT1M")); }
-            }
+if ($result !== false)
+{
+    if ($result !== NULL)
+    {
+        // Go back a minute if no record and not in absolute mode
+        if (!isset($_GET["abs"]))
+        {
+            $url_time->sub(new DateInterval("PT1M"));
+            $result = record_for_time($pdo, $url_time, DbTable::ENVREPORTS);
 
-        } else {
-            // Add record data to final data
-            foreach ($row = $record -> fetch_assoc() as $key => $value) {
-                if (array_key_exists($key, $data)) { $data[$key] = $value; }
-            }
+            if ($result !== false && $result !== NULL)
+            {
+                // Add result data to return data
+                foreach ($result as $key => $value)
+                {
+                    if (array_key_exists($key, $data))
+                        $data[$key] = $value;
+                }
+            } else $url_time->add(new DateInterval("PT1M"));
+        }
+    }
+    else
+    {
+        // Add result data to return data
+        foreach ($result as $key => $value)
+        {
+            if (array_key_exists($key, $data))
+                $data[$key] = $value;
         }
     }
 }
 
-$data["Time"] = $url_time -> format("Y-m-d H:i:s");
+$data["Time"] = $url_time->format("Y-m-d H:i:s");
 echo json_encode($data, JSON_NUMERIC_CHECK);
