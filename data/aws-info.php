6918e2e6818c8471133a090542024d90e427b4ee
Henry Hunt <henryhunt.dev@gmail.com>
2019-06-13 05:49:39 -0500

    Added file to request AWS info from config file

---
+++ data/aws-info.php
@@ -0,0 +1,16 @@
+<?php
+include_once("../routines/config.php");
+
+$data = array_fill_keys(["Name", "TimeZone", "Latitude",
+    "Longitude", "Elevation"], null);
+
+$config = new Config("../config.ini");
+if (!$config) { echo json_encode($data); exit(); }
+
+$data["Name"] = $config->get_aws_name();
+$data["TimeZone"] = $config->get_aws_time_zone();
+$data["Latitude"] = $config->get_aws_latitude();
+$data["Longitude"] = $config->get_aws_longitude();
+$data["Elevation"] = $config->get_aws_elevation();
+
+echo json_encode($data, JSON_NUMERIC_CHECK);
