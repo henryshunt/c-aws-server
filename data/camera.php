d76fc300f39f08bfe6cf9426774a1792012cf8e7
Henry Hunt <henryhunt.dev@gmail.com>
2019-06-20 09:24:07 -0500

    Added solar info to camera page

--- data/camera.php
+++ data/camera.php
@@ -2,7 +2,7 @@
 date_default_timezone_set("UTC");
 include_once("../routines/config.php");
 
-$data = array_fill_keys(["Time", "CImg", "SRis", "SSet"], null);
+$data = array_fill_keys(["Time", "CImg", "SRis", "SSet", "Noon"], null);
 
 try { $config = new Config("../config.ini"); }
 catch (Exception $e)
@@ -18,6 +18,10 @@ if (isset($_GET["time"]))
     {
         $url_time = date_create_from_format(
             "Y-m-d\TH-i-s", $_GET["time"]);
+
+            $local_time = clone $url_time;
+            $local_time->setTimezone(
+                new DateTimeZone($config->get_aws_time_zone()));
     }
     catch (Exception $e) { echo json_encode($data); exit(); }
 }
@@ -42,6 +46,28 @@ if (!file_exists($image_path))
     }
 }
 else $data["CImg"] = "data/" . $image_path;
+
+// Calculate sunrise and sunset times
+$solar_info = date_sun_info($local_time->getTimestamp(),
+    $config->get_aws_latitude(), $config->get_aws_longitude());
+
+if ($solar_info["sunrise"] !== true && $solar_info["sunrise"] !== false)
+{
+    $sunrise = $solar_info["sunrise"];
+    $data["SRis"] = (new DateTime("@$sunrise"))->format("Y-m-d H:i:s");
+}
+
+if ($solar_info["sunset"] !== true && $solar_info["sunset"] !== false)
+{
+    $sunset = $solar_info["sunset"];
+    $data["SSet"] = (new DateTime("@$sunset"))->format("Y-m-d H:i:s");
+}
+
+if ($solar_info["transit"] !== true && $solar_info["transit"] !== false)
+{
+    $noon = $solar_info["transit"];
+    $data["Noon"] = (new DateTime("@$noon"))->format("Y-m-d H:i:s");
+}
 
 $data["Time"] = $url_time->format("Y-m-d H:i:s");
 echo json_encode($data, JSON_NUMERIC_CHECK);
