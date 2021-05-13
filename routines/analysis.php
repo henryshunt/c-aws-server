0ff8a05c556ae5baad75f609b3ad8f80c9c45644
Henry Hunt <henryhunt.dev@gmail.com>
2021-05-13 01:50:55 +0100

    Removed some old code

--- routines/analysis.php
+++ routines/analysis.php
@@ -1,60 +1,6 @@
 <?php
 date_default_timezone_set("UTC");
 
-function record_for_time($pdo, $time, $table)
-{
-    $QUERY = "SELECT * FROM %s WHERE %s = ?";
-
-    try
-    {
-        if ($table == DbTable::DAYSTATS)
-        {
-            $query = query_database($pdo, sprintf($QUERY, $table, "Date"),
-                [$time->format("Y-m-d")]);
-        }
-        else
-        {
-            $query = query_database($pdo, sprintf($QUERY, $table, "Time"),
-                [$time->format("Y-m-d H:i:s")]);
-        }
-        
-        if ($query)
-        {
-            $result = $query->fetch();
-            return empty($result) ? NULL : $result;
-        } else return false;
-    }
-    catch (Exception $e) { return false; }
-}
-
-function fields_in_range($pdo, $start, $end, $fields, $table)
-{
-    $QUERY = "SELECT %s FROM %s WHERE %s BETWEEN ? AND ? ORDER BY %s";
-
-    try
-    {
-        if ($table == DbTable::DAYSTATS)
-        {
-            $query = query_database($pdo,
-                sprintf($QUERY, $fields, $table, "Date", "Date"),
-                [$start->format("Y-m-d"), $end->format("Y-m-d")]);
-        }
-        else
-        {
-            $query = query_database($pdo,
-                sprintf($QUERY, $fields, $table, "Time", "Time"),
-                [$start->format("Y-m-d H:i:s"), $end->format("Y-m-d H:i:s")]);
-        }
-
-        if ($query)
-        {
-            $result = $query->fetchAll();
-            return empty($result) ? NULL : $result;
-        } else return false;
-    }
-    catch (Exception $e) { return false; }
-}
-
 function stats_for_year($config, $pdo, $year)
 {
     if ($config->get_is_remote())
@@ -127,27 +73,6 @@ function stats_for_months($config, $pdo, $year)
         if ($query)
         {
             $result = $query->fetchAll();
-            return empty($result) ? NULL : $result;
-        } else return false;
-    }
-    catch (Exception $e) { return false; }
-}
-
-function past_hour_total($pdo, $time, $column)
-{
-    $QUERY = "SELECT SUM(%s) AS %s_PHr FROM reports WHERE Time BETWEEN ? AND ?";
-    $past_hour = clone $time;
-    $past_hour->sub(new DateInterval("PT59M"));
-
-    try
-    {
-        $query = query_database($pdo, sprintf($QUERY,
-            $column, $column), [$past_hour->format("Y-m-d H:i:s"),
-            $time->format("Y-m-d H:i:s")]);
-
-        if ($query)
-        {
-            $result = $query->fetch();
             return empty($result) ? NULL : $result;
         } else return false;
     }
